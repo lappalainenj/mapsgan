db6bdf0a46ad676dda24b3acff4649a452ce3346
Yağmur Yener <yagmur.yener.yy@gmail.com>
2019-02-04 13:39:02 +0100

    bicy hyp

---
+++ scripts/train_cloud_hyp_bicy.py
@@ -0,0 +1,48 @@
+import torch
+from mapsgan import BicycleSolver, BicycleGenerator, ToyGenerator, ToyDiscriminator, data_loader
+import mapsgan.experiments as experiments
+import time
+import datetime
+
+mode = 'cvae'
+fileprefix_ = '/cloud/hyp_bicy'
+lr_gen = 1e-4
+lr_dis = 1e-4
+lr_enc = 1e-4
+list_weights = [ {'disc': 1, 'traj': 2, 'kl': 0.1, 'z': 0.5},
+                 {'disc': 1, 'traj': 2, 'kl': 0.1, 'z': 0.5},
+                 {'disc': 1, 'traj': 2, 'kl': 0.1, 'z': 0.5}]
+
+
+#loss_weights={'disc': 1, 'traj': 2, 'kl': 0.1, 'z': 0.5}
+
+if torch.cuda.is_available():
+    print('Cuda is available')
+else:
+    print('Cuda is NOT available')
+
+print('Loading dataset...')
+experiment = experiments.ETH() # we store filepaths and arguments in here
+dataset, trainloader = data_loader(in_len=8, out_len=12, batch_size=64, num_workers=1,
+                                   path=experiment.train_dir, shuffle=True)
+
+
+for it, loss_weights in enumerate(list_weights):
+    fileprefix = fileprefix_ + '_' + str(it)
+
+    generator = BicycleGenerator(generator=ToyGenerator, start_mode=mode)
+    discriminator = ToyDiscriminator()
+
+    solver = BicycleSolver(generator, discriminator,
+                    loss_weights=loss_weights,
+                    optims_args={'generator': {'lr': lr_gen}, 'discriminator': {'lr': lr_dis}, 'encoder': {'lr': lr_enc}})
+
+    print('\nIteration ', it + 1, '/', len(list_weights))
+    print(datetime.datetime.now())
+    time_start = time.time()
+    solver.train(trainloader, epochs = 10, checkpoint_every=9, print_every=99, val_every=9,
+                 steps={'generator': 1, 'discriminator': 1},
+                 save_model=False, model_name=fileprefix, save_every=10000, restore_checkpoint_from=None)
+    time_elapsed = (time.time() - time_start)/60
+    print('End of training. Duration: ' + str(time_elapsed) + 'mins\n-------------------------------------------------')
+print('End of loop')
